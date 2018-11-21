ef5e14dbfe9c3dd1c92e12f89202d0c09f38f59b
huangchenjian <2118202015@tju.edu.cn>
2018-11-21 20:02:21 +0800

    2018年11月21日 第一次提交

---
+++ dev_SyntecCNC-Hanhai/SyntecCNC-Hanhai/CNC_Control.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace SyntecCNC_Hanhai
+{
+    partial class CNC_Control
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.LogBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.test_but = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(9, 44);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(126, 124);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CNC列表";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(498, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(60, 22);
+            this.toolStripButton1.Text = "添加机台";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(72, 22);
+            this.toolStripButton2.Text = "数据库设置";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // LogBox1
+            // 
+            this.LogBox1.Location = new System.Drawing.Point(138, 44);
+            this.LogBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.LogBox1.Multiline = true;
+            this.LogBox1.Name = "LogBox1";
+            this.LogBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LogBox1.Size = new System.Drawing.Size(352, 262);
+            this.LogBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "运行日志";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-2, 304);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "20181101A";
+            // 
+            // test_but
+            // 
+            this.test_but.Location = new System.Drawing.Point(9, 172);
+            this.test_but.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.test_but.Name = "test_but";
+            this.test_but.Size = new System.Drawing.Size(124, 18);
+            this.test_but.TabIndex = 6;
+            this.test_but.Text = "通讯自检";
+            this.test_but.UseVisualStyleBackColor = true;
+            this.test_but.Click += new System.EventHandler(this.test_but_Click);
+            // 
+            // CNC_Control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 315);
+            this.Controls.Add(this.test_but);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LogBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "CNC_Control";
+            this.Text = "瀚海CNC自动控制";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CNC_Control_FormClosing);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.TextBox LogBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button test_but;
+    }
+}
+
---
+++ dev_SyntecCNC-Hanhai/SyntecCNC-Hanhai/CNC_Control.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
+using Syntec.Remote;
+using System.Threading;
+
+namespace SyntecCNC_Hanhai
+{
+    public partial class CNC_Control : Form
+    {
+
+        List<SyntecRemoteCNC> m_CNC;
+        MySqlConnection mysql;
+        SynchronizationContext m_SyncContext = null;
+
+        public CNC_Control()
+        {
+            InitializeComponent();
+            m_SyncContext = SynchronizationContext.Current;
+            mysql = getMySqlCon();
+            m_CNC = new List<SyntecRemoteCNC>();
+            SyntecRemoteCNC cnc = new SyntecRemoteCNC(CNCIP.Default.一号机);
+            //SyntecRemoteCNC cnc = new SyntecRemoteCNC("127.0.0.1");
+            m_CNC.Add(cnc);
+            cnc = new SyntecRemoteCNC(CNCIP.Default.二号机);
+            m_CNC.Add(cnc);
+            cnc = new SyntecRemoteCNC(CNCIP.Default.三号机);
+            m_CNC.Add(cnc);
+            cnc = new SyntecRemoteCNC(CNCIP.Default.四号机);
+            m_CNC.Add(cnc);
+            cnc = new SyntecRemoteCNC(CNCIP.Default.五号机);
+            m_CNC.Add(cnc);
+            cnc = new SyntecRemoteCNC(CNCIP.Default.六号机);
+            m_CNC.Add(cnc);
+            cnc = new SyntecRemoteCNC(CNCIP.Default.七号机);
+            m_CNC.Add(cnc);
+            cnc = new SyntecRemoteCNC(CNCIP.Default.八号机);
+            m_CNC.Add(cnc);
+            int i = 0;
+            foreach (SyntecRemoteCNC tmp in m_CNC)
+            {
+                i++;
+                listBox1.Items.Add(i+"号:"+tmp.Host);
+            }
+            /*OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+                MessageBox.Show(ofd.FileName);*/
+            Thread sql_thread = new Thread(new ThreadStart(DBListen));
+            sql_thread.Start();
+        }
+
+        public MySqlConnection getMySqlCon()
+        {
+            String mysqlStr = "Database='" + DBConfig.Default.NAME + "';Data Source='" + DBConfig.Default.IP + "';User Id='" + DBConfig.Default.USER + "';Password='" + DBConfig.Default.PASS + "';CharSet='utf8'";
+            MySqlConnection mysql = new MySqlConnection(mysqlStr);
+            return mysql;
+        }
+
+        public void DBListen()
+        {
+            while (true)
+            {
+                int Index = 0;
+                Boolean Success_flag = false;
+                String SQLSearch = "SELECT `Machine_num`,`NC_PATH`,`Index` FROM `work_cnc_task_list` WHERE `CNC`=5";
+                MySqlCommand mySqlCommand = new MySqlCommand(SQLSearch, mysql);
+                try
+                {
+                    mysql.Open();
+                    MySqlDataReader reader = mySqlCommand.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        if (reader.HasRows)
+                        {
+                            int Machine_num = reader.GetInt32(0);
+                            String Filepath = reader.GetString(1);
+                         
+                            Index = reader.GetInt32(2);
+                            m_SyncContext.Post(Log, "准备上传文件" + Filepath + "至" + Machine_num +"号机床！");
+                            SyntecRemoteCNC cnc = m_CNC[Machine_num - 1];
+                            short result = cnc.UPLOAD_nc_mem(Filepath);
+                            
+                            if (result == (short)SyntecRemoteCNC.ErrorCode.NormalTermination)
+                            {
+                                WaitForUploadFile(cnc);
+                            }
+                            else
+                            {
+                                result = cnc.UPLOAD_nc_mem(Filepath);
+                                if (result == (short)SyntecRemoteCNC.ErrorCode.NormalTermination)
+                                {
+                                    WaitForUploadFile(cnc);
+                                }
+                                else
+                                    m_SyncContext.Post(Log, "NC文件上传失败！" + result);
+                            }
+
+                            string filename = Filepath.Substring(Filepath.LastIndexOf("\\") + 1, Filepath.LastIndexOf(".") - (Filepath.LastIndexOf("\\") + 1)) +"."+ Filepath.Substring(Filepath.LastIndexOf(".") + 1, Filepath.Length - Filepath.LastIndexOf(".") - 1);
+                            result = cnc.WRITE_nc_main(filename);//上传指定
+                            int fail_num = 0;
+                            int CurSeq = 0;
+                            string MainProg = "", CurProg = "", Mode = "", Status = "", Alarm = "", EMG = "";
+                            short result1 = cnc.READ_status(out MainProg, out CurProg, out CurSeq, out Mode, out Status, out Alarm, out EMG);
+        
+                            if (result == (short)SyntecRemoteCNC.ErrorCode.NormalTermination&&MainProg==filename)
+                            {
+                                m_SyncContext.Post(Log, cnc.Host + ":加工文件指定成功:" + filename);
+                                Success_flag = true;
+                            }
+                            else
+                            {
+                                while (MainProg != filename)
+                                {
+                                    Console.WriteLine("ERROR");
+                                    cnc.WRITE_nc_main(filename);
+                                    cnc.READ_status(out MainProg, out CurProg, out CurSeq, out Mode, out Status, out Alarm, out EMG);
+                                    Thread.Sleep(10);
+                                    fail_num++;
+                                    if (fail_num==100)
+                                    {
+                                        m_SyncContext.Post(Log, cnc.Host + ":加工文件指定失败:" + result.ToString() + "-" + filename);
+                                        break;
+                                    }
+                                }
+                                m_SyncContext.Post(Log, cnc.Host + ":加工文件指定成功:" + filename);
+                                Success_flag = true;
+                            }
+                        }
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    m_SyncContext.Post(Log, "数据库读取失败！原因：" + ex.ToString());
+                }
+                finally
+                {
+                    mysql.Close();
+                }
+                if(Success_flag)
+                    Sql_do("UPDATE `work_cnc_task_list` SET `CNC`=10 WHERE `Index`=" + Index);
+                else
+                    Sql_do("UPDATE `work_cnc_task_list` SET `CNC`=1000 WHERE `Index`=" + Index);
+                Thread.Sleep(10);
+            }
+        }
+
+        private void WaitForUploadFile(SyntecRemoteCNC cnc)
+        {
+            while (!cnc.isFileUploadDone)
+            {
+
+                Thread.Sleep(50);
+                Console.WriteLine("try_load");
+            }
+            m_SyncContext.Post(Log, cnc.Host + " 加工文件上传成功！");
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)//工具栏添加机台
+        {
+            string cncip = Interaction.InputBox("请输入机台IP", "添加机台", "192.168.31.XXX", 100, 100);
+
+            SyntecRemoteCNC cnc = new SyntecRemoteCNC(cncip);
+            m_CNC.Add(cnc);
+            listBox1.Items.Add(cnc.Host);
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)//工具栏数据库设置
+        {
+            DBConfig.Default.IP = Interaction.InputBox("请输入服务器IP", "数据库服务器修改", DBConfig.Default.IP, 100, 100);
+            DBConfig.Default.NAME = Interaction.InputBox("请输入数据库名", "数据库服务器修改", DBConfig.Default.NAME, 100, 100);
+            DBConfig.Default.USER = Interaction.InputBox("请输入登陆用户名", "数据库服务器修改", DBConfig.Default.USER, 100, 100);
+            DBConfig.Default.PASS = Interaction.InputBox("请输入登陆密码", "数据库服务器修改", DBConfig.Default.PASS, 100, 100);
+            DBConfig.Default.Save();
+            mysql = getMySqlCon();
+        }
+
+        public void Log(Object log_info)
+        {
+            String tolog = (String)log_info;
+            LogBox1.AppendText(tolog + "\r\n");
+            LogBox1.ScrollToCaret();
+        }
+
+        private bool Sql_do(String command)
+        {
+            MySqlCommand mySqlCommand = new MySqlCommand(command, mysql);
+            try
+            {
+                mysql.Open();
+                mySqlCommand.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("MySqlException Error3:" + ex.ToString());
+                return true;
+            }
+            finally
+            {
+                mysql.Close();
+            }
+            return true;
+        }
+
+        private void CNC_Control_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void test_but_Click(object sender, EventArgs e)
+        {
+            foreach( SyntecRemoteCNC cnc in m_CNC ) {
+				int CurSeq = 0;
+				string MainProg = "", CurProg = "", Mode = "", Status = "", Alarm = "", EMG = "";
+
+				short result = cnc.READ_status( out MainProg, out CurProg, out CurSeq, out Mode, out Status, out Alarm, out EMG );
+				if( result == (short)SyntecRemoteCNC.ErrorCode.NormalTermination ) {
+					Log( "===" + cnc.Host + "===" );
+					Log( "MainProg : " + MainProg.ToString() );
+					Log( "CurrentProg : " + CurProg.ToString() );
+					Log( "CurrentSeq : " + CurSeq.ToString() );
+					Log( "Mode : " + Mode.ToString() );
+					Log( "Status : " + Status.ToString() );
+					Log( "Alarm : " + Alarm.ToString() );
+					Log( "EMG : " + EMG.ToString() );
+				}
+				else {
+					Log( cnc.Host + " : Error:testREAD_status:" + result.ToString() );
+				}
+			}
+            
+        }
+
+
+    }
+}
